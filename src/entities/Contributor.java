c2f8cc2e8dc65e470cbc9c984d3091e73fbf419b
CaioRufino233 <caiorufinodacosta@outlook.com.br>
2023-10-11 02:13:13 -0300

    Adding my repository with abstract classes

---
+++ src/entities/Contributor.java
@@ -0,0 +1,32 @@
+package entities;
+
+public abstract class Contributor {
+    protected String name;
+    protected Double anualIncome;
+
+    public Contributor() {
+    }
+
+    public Contributor(String name, Double anualIncome) {
+        this.name = name;
+        this.anualIncome = anualIncome;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnualIncome() {
+        return anualIncome;
+    }
+
+    public void setAnualIncome(Double anualIncome) {
+        this.anualIncome = anualIncome;
+    }
+
+    public abstract Double tax();
+}
---
+++ src/entities/LegalPerson.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class LegalPerson extends Contributor{
+
+
+    @Override
+    public Double tax() {
+        return null;
+    }
+}
